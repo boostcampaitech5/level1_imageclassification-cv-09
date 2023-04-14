2695598e1ff0e6854687bd3cab03c72b3a7bc593
hyuns <hyuns6677@gmail.com>
2023-04-14 11:19:40 +0000

    asdf

--- dataset.py
+++ dataset.py
@@ -1,1 +1,20 @@
-# dataset
+from torch.utils.data import Dataset
+import os
+
+
+class CustomDataset(Dataset):
+    def __init__(self, data_dir, transform):
+        self.data_dir = data_dir
+        self.transform = transform
+        self.image_files = os.listdir(data_dir)
+
+    def __len__(self):
+        # 데이터셋의 총 데이터 수 반환
+        for data in self.image_files:
+            print(data)
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        # idx 번째 데이터 반환
+        # 예시) return self.data[idx]
+        return self.image_files[idx]
