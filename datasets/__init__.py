4c248d1e05d54b9dae3e9980b48c339895d7d356
HyeonJun <guswns9579@naver.com>
2023-04-17 00:58:30 +0000

    model_soup change

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,7 +2,7 @@
 Datasets is based on https://github.com/mlfoundations/wise-ft/tree/master/src/datasets
 '''
 from .imagenet import *
-from .imagenetv2 import ImageNetV2
+#from .imagenetv2 import ImageNetV2
 from .imagenet_a import ImageNetAValClasses, ImageNetA
 from .imagenet_r import ImageNetRValClasses, ImageNetR
 from .objectnet import ObjectNetValClasses, ObjectNet
