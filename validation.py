5d3dfaa420ebdd4dc9a22c18f482f7753cb1f97c
Hyunmin-H <handy389@naver.com>
2023-04-18 06:49:17 +0000

    Debug & Add val_img directory per model name

--- validation.py
+++ validation.py
@@ -16,9 +16,13 @@ from datasets.imagenet import ImageNet98p, ImageNet
 from datasets.maskbasedataset import MaskBaseDataset, BaseAugmentation, get_transforms
 from utils import ModelWrapper, maybe_dictionarize_batch, cosine_lr, get_model_from_sd
 from zeroshot import zeroshot_classifier
-from inference import load_model
 import torchvision.transforms.functional as TF
 import copy
+
+
+#############입력하세요#############
+model_name = 'finetuned_centercrop3000_epoch10.pt'
+###################################
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -82,7 +86,6 @@ def parse_arguments():
 
     return parser.parse_args()
 
-    
 def grid_image(np_images, gts, preds, is_wrong, n=16):
     batch_size = np_images.shape[0]
     index = is_wrong.nonzero()
@@ -112,16 +115,20 @@ def grid_image(np_images, gts, preds, is_wrong, n=16):
 
     return figure
 
+def load_model(saved_model, num_classes, device):
+    model_path = os.path.join('/opt/ml/level1_imageclassification-cv-09/model', model_name)
+    base_model, preprocess = clip.load('ViT-B/32', 'cuda', jit=False)
+    print('model_path', model_path)
+    state_dict = torch.load(model_path, map_location=torch.device('cpu'))
+    model = get_model_from_sd(state_dict, base_model)
+
+    return model
 
 if __name__ == '__main__':
     
     args = parse_arguments()
     DEVICE = 'cuda'
     
-    #############입력하세요#############
-    model_name = 'finetuned_centercrop3000_epoch10.pt'
-    ###################################
-
     model_path = os.path.join(args.model_location, model_name) 
     model = load_model(model_path, 18, 'cuda').to('cuda')
 
@@ -243,7 +250,10 @@ if __name__ == '__main__':
             inputs_np = MaskBaseDataset.denormalize_image(inputs_np, dataset.mean, dataset.std)
 
             figure = grid_image(inputs_np, labels, pred, is_wrong, n=25) # 16
-            figure.savefig(f'val_img/my_plot_batch{i}.png')
+
+            dir = f'val_img/{model_name}'
+            os.makedirs(dir, exist_ok=True)
+            figure.savefig(f'{dir}/my_plot_batch{i}.png')
 
         top1 = correct / count
         wrong_percent = wrong_percent.to(torch.float)
