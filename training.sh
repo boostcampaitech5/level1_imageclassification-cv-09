312a060b5df31adfd7e9cda4c74bddc330c7e4b4
jibeomkim7 <jibeomkim7@gmail.com>
2023-04-19 11:46:54 +0000

    old_data 추가

--- training.sh
+++ training.sh
@@ -1,6 +1,6 @@
-python finetune.py --name normal --random-seed 42 --i 0
-python finetune.py --name normal --random-seed 34 --i 1
-python finetune.py --name normal --random-seed 48 --i 2
-python finetune.py --name normal --random-seed 23 --i 3
+python finetune.py --name add_old_data --random-seed 42 --i 0
+python finetune.py --name add_old_data --random-seed 34 --i 1
+python finetune.py --name add_old_data --random-seed 48 --i 2
+python finetune.py --name add_old_data --random-seed 23 --i 3
 
 # bash training.sh
