5d20686caaf3c2e8ee4e13659d62513aa9e0049d
Hyunmin-H <handy389@naver.com>
2023-04-25 03:07:13 +0000

    Add weighted average ensemble, soft voting, contrastive loss & code cleanup

--- inference.py
+++ inference.py
@@ -10,21 +10,12 @@ from torch.utils.data import DataLoader
 from datasets.maskbasedataset import TestDataset, MaskBaseDataset
 import clip
 import math
-# from utils import get_model_from_sd
 
-###############입력하세요##############
-model_name = 'finetuned_aug_combine0_epoch10.pt'
-######################################
-
-def load_model(saved_model, num_classes, device):
-
-    model_path = os.path.join('/opt/ml/model-soups/model', model_name)
-    base_model, preprocess = clip.load('ViT-B/32', 'cuda', jit=False)
+def load_model(model_path, device, model):
+    base_model, preprocess = clip.load(model, 'cuda', jit=False)
     state_dict = torch.load(model_path, map_location=torch.device('cpu'))
     model = get_model_from_sd(state_dict, base_model)
-
     return model
-
 
 @torch.no_grad()
 def inference(data_dir, model_dir, output_dir, args):
@@ -34,7 +25,9 @@ def inference(data_dir, model_dir, output_dir, args):
     device = torch.device("cuda" if use_cuda else "cpu")
 
     num_classes = MaskBaseDataset.num_classes  # 18
-    model = load_model(model_dir, num_classes, device).to(device)
+    
+    model_path = os.path.join(args.model_location, args.model_name) 
+    model = load_model(model_path, device, args.model).to(device)
     model.eval()
 
     img_root = os.path.join(data_dir, 'images')
@@ -52,17 +45,58 @@ def inference(data_dir, model_dir, output_dir, args):
         drop_last=False,
     )
 
+    if args.weighted_ensemble  :
+        model_path2 = os.path.join(args.model_location, args.weighted_ensemble) 
+        model2 = load_model(model_path2, device, args.model).to(device)
+        model2.eval()
+        print('Weighted average Ensemble')
+    if args.soft_voting: 
+        model_path2 = os.path.join(args.model_location, args.soft_voting) 
+        model2 = load_model(model_path2, device, args.model).to(device)
+        model2.eval()
+        print('Soft Voting')
+
     print("Calculating inference results..")
     preds = []
     with torch.no_grad():
         for idx, images in enumerate(loader):
             images = images.to(device)
             pred = model(images)
+
+            if args.weighted_ensemble :
+                pred2 = model2(images)
+                for i in range(len(pred)): 
+                    
+                    age1_percent = 1. + pred2[i][0] / pred2[i].sum()
+                    age2_percent = 1. + pred2[i][1] / pred2[i].sum()
+                    age3_percent = 1. + pred2[i][2] / pred2[i].sum()
+                    
+                    age_percent = torch.tensor([age1_percent, age2_percent, age3_percent,
+                        age1_percent, age2_percent, age3_percent,
+                        age1_percent, age2_percent, age3_percent,
+                        age1_percent, age2_percent, age3_percent,
+                        age1_percent, age2_percent, age3_percent,
+                        age1_percent, age2_percent, age3_percent] , device='cuda')
+
+                    pred[i] *= age_percent
+
+            if args.soft_voting: 
+                pred2  = model2(images)
+                for i in range(len(pred)): 
+                    pred[i] = (pred[i] - pred[i].min()) / (pred[i].max() - pred[i].min())
+                    pred2[i] = (pred2[i] - pred2[i].min()) / (pred2[i].max() - pred2[i].min())
+                    pred[i] += pred2[i]
+
             pred = pred.argmax(dim=-1)
             preds.extend(pred.cpu().numpy())
 
     info['ans'] = preds
-    save_path = os.path.join(output_dir, f'output_{model_name}.csv')
+    if args.weighted_ensemble : 
+        save_path = os.path.join(output_dir, f'output_{args.model_name[:-3]}_WE_{args.weighted_ensemble[:-3]}.csv')
+    elif args.soft_voting : 
+        save_path = os.path.join(output_dir, f'output_{args.model_name[:-3]}_SV_{args.soft_voting[:-3]}.csv')
+    else : 
+        save_path = os.path.join(output_dir, f'output_{args.model_name[:-3]}.csv')
     info.to_csv(save_path, index=False)
     print(f"Inference Done! Inference result saved at {save_path}")
 
@@ -118,17 +152,22 @@ if __name__ == '__main__':#####################################resize default값
     # Data and model checkpoints directories
     parser.add_argument('--batch_size', type=int, default=1000, help='input batch size for validing (default: 1000)')
     parser.add_argument('--resize', type=tuple, default=(224, 224), help='resize size for image when you trained (default: (96, 128))')
-    parser.add_argument('--model', type=str, default='BaseModel', help='model type (default: BaseModel)')
+    parser.add_argument('--model', type=str, default='ViT-B/32', help='model type (default: BaseModel)')
 
     # Container environment
     parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_EVAL', '/opt/ml/input/data/eval'))
-    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_CHANNEL_MODEL', './model/exp'))
+    parser.add_argument('--model-location', type=str, default=os.environ.get('SM_CHANNEL_MODEL', './model'))
     parser.add_argument('--output_dir', type=str, default=os.environ.get('SM_OUTPUT_DATA_DIR', './output'))
+    parser.add_argument("--weighted-ensemble", default=None, help='Filename of age model to apply weighted average ensemble')
+    parser.add_argument("--soft-voting", default=None, help='Write file name of model to apply soft voting ensemble')
+    parser.add_argument("--model-name", default=None, help='Filename of model')
 
     args = parser.parse_args()
 
+    assert not (args.weighted_ensemble and args.soft_voting), "Activate one either Weighted Average Ensemble or Soft Voting "
+
     data_dir = args.data_dir
-    model_dir = args.model_dir
+    model_dir = args.model_location
     output_dir = args.output_dir
 
     os.makedirs(output_dir, exist_ok=True)
--- main.py
+++ main.py
@@ -22,13 +22,13 @@ def parse_arguments():
     parser.add_argument(
         "--data-location",
         type=str,
-        default=os.path.expanduser('~/data'),
+        default=os.path.expanduser('/opt/ml/input/data/train/images/'),
         help="The root directory for the datasets.",
     )
     parser.add_argument(
         "--model-location",
         type=str,
-        default=os.path.expanduser('/opt/ml/model-soups/model'),
+        default=os.path.expanduser('model/'),
         help="Where to download the models.",
     )
     parser.add_argument(
@@ -56,7 +56,6 @@ def parse_arguments():
         type=int,
         default=4,
     )
-    
     parser.add_argument(
         "--random-seed", 
         type=int,
@@ -66,16 +65,29 @@ def parse_arguments():
         "--name",
         default='finetune_cp'
     )
+    parser.add_argument(
+        "--model-num", 
+        type=int,
+        default=40,
+    )
+    parser.add_argument(
+        "--epoch", 
+        type=int,
+        default=20,
+    )
+    parser.add_argument(## default 0.2 / None값을 넣는다면, 전체 dataset에 대해 evaluation 진행
+        "--val-ratio", 
+        type=float,
+        default=0.2,
+    )
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_arguments()
 
-    ###############입력하세요##############
-    NUM_MODELS = 40
-    epoch = 20
-    val_ratio=0.2 ## default 0.2 / None값을 넣는다면, 전체 dataset에 대해 evaluation 진행
-    ######################################
+    NUM_MODELS = args.model_num
+    epoch = args.epoch
+    val_ratio=args.val_ratio 
     
     model_name = args.name 
 
@@ -89,8 +101,8 @@ if __name__ == '__main__':
         random.seed(args.random_seed)
 
 
-    INDIVIDUAL_MODEL_RESULTS_FILE = f'logs/individual_results_{model_name}_num{NUM_MODELS}_epoch{epoch}.jsonl'
-    GREEDY_SOUP_LOG_FILE = f'logs/greedy_soup_log_{model_name}_num{NUM_MODELS}_epoch{epoch}.txt'
+    INDIVIDUAL_MODEL_RESULTS_FILE = f'logs/individual_results_{model_name}_seed{args.random_seed}_num{NUM_MODELS}_epoch{epoch}.jsonl'
+    GREEDY_SOUP_LOG_FILE = f'logs/greedy_soup_log_{model_name}_seed{args.random_seed}_num{NUM_MODELS}_epoch{epoch}.txt'
 
     UNIFORM_SOUP_RESULTS_FILE = 'logs/uniform_soup_results.jsonl'
     GREEDY_SOUP_RESULTS_FILE = 'logs/greedy_soup_results.jsonl'
@@ -106,7 +118,7 @@ if __name__ == '__main__':
                 out=args.model_location
                 )
 
-    model_paths = [os.path.join(args.model_location, f'{model_name}{i}_epoch{epoch}.pt') for i in range(NUM_MODELS)]
+    model_paths = [os.path.join(args.model_location, f'{model_name}_seed{args.random_seed}_i{i}_epoch{epoch}.pt') for i in range(NUM_MODELS)]
     # Step 2: Evaluate individual models.
     if args.eval_individual_models or args.uniform_soup or args.greedy_soup:
         base_model, preprocess = clip.load('ViT-B/32', 'cpu', jit=False)
@@ -119,8 +131,7 @@ if __name__ == '__main__':
             state_dict = torch.load(model_path, map_location=torch.device('cpu'))
 
             model = get_model_from_sd(state_dict, base_model)
-
-            results = {'model_name' : f'{model_name}{j}_epoch{epoch}'}
+            results = {'model_name' : f'{model_name}_seed{args.random_seed}_i{j}_epoch{epoch}'}
             # Note: ImageNet2p is the held-out minival set from ImageNet train that we use.
             # It is called 2p for 2 percent of ImageNet, or 26k images.
             # See utils on how this dataset is handled slightly differently.
@@ -223,9 +234,8 @@ if __name__ == '__main__':
 
         # Finally, evaluate the greedy soup.
         model = get_model_from_sd(greedy_soup_params, base_model)
-
         # save final model
-        model_path = os.path.join(args.model_location, f'{model_name}_epoch{epoch}_greedysoup_num{NUM_MODELS}.pt')
+        model_path = os.path.join(args.model_location, f'{model_name}_seed{args.random_seed}_epoch{epoch}_greedysoup_num{NUM_MODELS}.pt')
         print('Saving model to', model_path)
         torch.save(model.module.state_dict(), model_path)
 
